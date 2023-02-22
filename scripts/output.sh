68788f2c39c908042bce3bea4ce12a51877e90a2
ferrreo <harderthanfire@gmail.com>
2023-02-22 16:55:06 +0000

    Add release build

--- scripts/output.sh
+++ scripts/output.sh
@@ -3,6 +3,5 @@
 echo "Pika Kernel - Copying Output"
 
 cd ..
-mkdir ./output
 rm ./linux-libc*.deb
 cp ./*.deb ./output/
--- scripts/release.sh
+++ scripts/release.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
 echo "Pika Kernel - Releasing Kernel"
+
+# Sign the packages
+dpkg-sig --sign builder ./output/linux-image*.deb
+dpkg-sig --sign builder ./output/linux-headers*.deb
+
+# Update the repo
+reprepro -V --basedir ./output/repo/ includedeb kinetic ./output/linux-image*.deb
+reprepro -V --basedir ./output/repo/ includedeb kinetic ./output/linux-headers*.deb
+
+gpg --armor --export harderthanfire@gmail.com | tee ./output/repo/key.gpg
+
+rsync -azP --delete ./output/repo/ pikappa@pika-os.com:/srv/www/pikappa/
