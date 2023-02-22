ee4f3a81f3479331c6ac559ec8e4fce1ac0053a7
ferrreo <harderthanfire@gmail.com>
2023-02-22 18:15:27 +0000

    static keyfile for repo instead of generating each time

--- scripts/release.sh
+++ scripts/release.sh
@@ -10,6 +10,4 @@ dpkg-sig --sign builder ./output/linux-headers*.deb
 reprepro -V --basedir ./output/repo/ includedeb kinetic ./output/linux-image*.deb
 reprepro -V --basedir ./output/repo/ includedeb kinetic ./output/linux-headers*.deb
 
-gpg --armor --export harderthanfire@gmail.com | tee ./output/repo/key.gpg
-
 rsync -azP ./output/repo/ ferreo@pika-os.com:/srv/www/pikappa/
