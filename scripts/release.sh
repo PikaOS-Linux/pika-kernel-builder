2de19d510f1d8ba172b40b4f44eb1f6723f6db5d
ferrreo <harderthanfire@gmail.com>
2023-02-24 12:30:29 +0000

    Fix re-adding new pacakge

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,12 +6,19 @@ echo "Pika Kernel - Releasing Kernel"
 dpkg-sig --sign builder ./output/linux-image*.deb
 dpkg-sig --sign builder ./output/linux-headers*.deb
 
+# Pull down existing ppa repo db files etc
 rsync -azP --exclude '*.deb' ferreo@pika-os.com:/srv/www/pikappa/ ./output/repo
 
+# Copy over our gpg key in case it has been updated
 cp ./output/key.gpg ./output/repo/key.gpg
 
-# Update the repo
+# Remove our existing package from the repo
+reprepro -Vb --basedir ./output/repo/ remove kinetic linux-image*
+reprepro -Vb --basedir ./output/repo/ remove kinetic linux-headers*
+
+# Add the new package to the repo
 reprepro -V --basedir ./output/repo/ includedeb kinetic ./output/linux-image*.deb
 reprepro -V --basedir ./output/repo/ includedeb kinetic ./output/linux-headers*.deb
 
+# Push the updated ppa repo to the server
 rsync -azP ./output/repo/ ferreo@pika-os.com:/srv/www/pikappa/
