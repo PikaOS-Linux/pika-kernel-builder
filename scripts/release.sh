63350f295c3a1a0159d15c210bdb22b68e4405af
ferrreo <harderthanfire@gmail.com>
2023-02-24 12:01:04 +0000

    Move key, update release so it updates the ppa repo instead of creating a new one each time

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,6 +6,10 @@ echo "Pika Kernel - Releasing Kernel"
 dpkg-sig --sign builder ./output/linux-image*.deb
 dpkg-sig --sign builder ./output/linux-headers*.deb
 
+rsync -azP --exclude '*.deb' ferreo@pika-os.com:/srv/www/pikappa/ ./output/repo
+
+cp ./output/key.gpg ./output/repo/key.gpg
+
 # Update the repo
 reprepro -V --basedir ./output/repo/ includedeb kinetic ./output/linux-image*.deb
 reprepro -V --basedir ./output/repo/ includedeb kinetic ./output/linux-headers*.deb
