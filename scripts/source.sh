ecf45f0674f3a449c3eda194b23e2affe2a63284
ferrreo <harderthanfire@gmail.com>
2023-03-14 08:42:46 +0000

    Update kernel to 6.2.6 for AMD TPM fix

--- scripts/source.sh
+++ scripts/source.sh
@@ -2,7 +2,7 @@
 
 echo "Pika Kernel - Getting source"
 
-wget -nv https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.2.5.tar.gz
-tar -zxf ./linux-6.2.5.tar.gz
+wget -nv https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.2.6.tar.gz
+tar -zxf ./linux-6.2.6.tar.gz
 
-cd linux-6.2.5
+cd linux-6.2.6
