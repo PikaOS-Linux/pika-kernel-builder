ac04591315e2f8517e89e3ef63c73c6ead4e31cb
ferrreo <harderthanfire@gmail.com>
2023-04-24 19:45:13 +0100

    Update for lunar

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,20 +5,21 @@ echo "Pika Kernel - Releasing Kernel"
 # Sign the packages
 dpkg-sig --sign builder ./output/linux-image*.deb
 dpkg-sig --sign builder ./output/linux-headers*.deb
-#dpkg-sig --sign builder ./output/linux-tools*.deb
+echo "Pika Kernel - Package sign complete"
 
 # Pull down existing ppa repo db files etc
 rsync -azP --exclude '*.deb' ferreo@direct.pika-os.com:/srv/www/pikappa/ ./output/repo
-
-# Copy over our gpg key in case it has been updated
-cp ./output/key.gpg ./output/repo/key.gpg
+echo "Pika Kernel - Repo rsync pull complete"
 
 # Remove our existing package from the repo - only for current version so we can update it
 reprepro -V --basedir ./output/repo/ removefilter lunar 'Package (% linux-*-6.3.0-pikaos*)'
+echo "Pika Kernel - Repo clean complete"
 
 # Add the new package to the repo
 reprepro -V --basedir ./output/repo/ includedeb lunar ./output/linux-image*.deb
 reprepro -V --basedir ./output/repo/ includedeb lunar ./output/linux-headers*.deb
+echo "Pika Kernel - Repo update complete"
 
 # Push the updated ppa repo to the server
 rsync -azP ./output/repo/ ferreo@direct.pika-os.com:/srv/www/pikappa/
+echo "Pika Kernel - Repo rsync push complete"
