5e6115edc056717f64aea1f576fec9fb0fab046b
ferrreo <harderthanfire@gmail.com>
2023-06-10 23:29:56 +0100

    Update release.sh

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,15 +1,11 @@
-#!/bin/bash
-
-echo "Pika Kernel - Releasing Kernel"
-
 # Sign the packages
 dpkg-sig --sign builder ./output/*.deb
 
 # Pull down existing ppa repo db files etc
-rsync -azP --exclude '*.deb' ferreo@direct.pika-os.com:/srv/www/pikappa/ ./output/repo
+rsync -azqP --exclude '*.deb' ferreo@direct.pika-os.com:/srv/www/pikappa/ ./output/repo
 
 # Add the new package to the repo
 reprepro -V --basedir ./output/repo/ includedeb lunar ./output/*.deb
 
 # Push the updated ppa repo to the server
-rsync -azP ./output/repo/ ferreo@direct.pika-os.com:/srv/www/pikappa/
+rsync -azqP ./output/repo/ ferreo@direct.pika-os.com:/srv/www/pikappa/
