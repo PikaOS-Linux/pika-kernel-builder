7f0d92efa72bfdec8da7d7b3672813bce44bc833
ferrreo <harderthanfire@gmail.com>
2023-06-15 19:19:12 +0100

    RC6

--- scripts/patch.sh
+++ scripts/patch.sh
@@ -7,8 +7,6 @@ echo "Pika Kernel - Applying patches"
 patch -Np1 < "../patches/0001-cachy-all.patch"
 # orig patch from cachy
 patch -Np1 < "../patches/0002-eevdfbore.patch"
-# orig patch from cachy
-patch -Np1 < "../patches/0003-bore.patch"
 # Nobara patches are here: https://github.com/sammilucia/nobara-kernel-fork
 # Allow setting custom pollrates for usb devices
 patch -Np1 < "../patches/0004-Allow-to-set-custom-USB-pollrate-for-specific-device.patch"
