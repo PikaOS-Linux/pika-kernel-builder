e02796144195568a79503dec33629249c6a9a1f2
ferrreo <harderthanfire@gmail.com>
2023-06-16 08:53:16 +0100

    BORE

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,4 +2,4 @@
 
 echo "Pika Kernel - Building"
 
-make -j`nproc` bindeb-pkg LOCALVERSION=-pikaos KDEB_PKGVERSION=$(make kernelversion)-1
+make -j`nproc` bindeb-pkg LOCALVERSION=-pikaos KDEB_PKGVERSION=$(make kernelversion)-2
