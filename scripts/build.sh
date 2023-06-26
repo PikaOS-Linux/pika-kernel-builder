aa7d9d711d88cafa6d2a5f16d3d3dc5c8cf11d84
ferrreo <harderthanfire@gmail.com>
2023-06-26 14:03:02 +0100

    6.4 release

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,4 +2,4 @@
 
 echo "Pika Kernel - Building"
 
-make -j`nproc` bindeb-pkg LOCALVERSION=-pikaos KDEB_PKGVERSION=$(make kernelversion)-2
+make -j`nproc` bindeb-pkg LOCALVERSION=-pikaos KDEB_PKGVERSION=$(make kernelversion)-1
--- scripts/source.sh
+++ scripts/source.sh
@@ -2,7 +2,7 @@
 
 echo "Pika Kernel - Getting source"
 
-wget -nv https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/snapshot/linux-6.4-rc6.tar.gz
-tar -xf ./linux-6.4-rc6.tar.gz
+wget -nv https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.4.tar.gz
+tar -xf ./linux-6.4.tar.gz
 
-cd linux-6.4-rc6
+cd linux-6.4
