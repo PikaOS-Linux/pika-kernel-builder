6dc67ea0fafe4bdf6d4dc721a4ec43049b76ff0a
ferrreo <harderthanfire@gmail.com>
2023-07-18 19:43:09 +0100

    6.4.3

--- scripts/source.sh
+++ scripts/source.sh
@@ -2,7 +2,7 @@
 
 echo "Pika Kernel - Getting source"
 
-wget -nv https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.4.1.tar.gz
-tar -xf ./linux-6.4.1.tar.gz
+wget -nv https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.4.3.tar.gz
+tar -xf ./linux-6.4.3.tar.gz
 
-cd linux-6.4.1
+cd linux-6.4.3
