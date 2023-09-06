4e54307971322800f0be484c3840178997db3ca6
ferrreo <harderthanfire@gmail.com>
2023-09-06 21:04:37 +0100

    Update release.sh

--- release.sh
+++ release.sh
@@ -1,14 +1,8 @@
-# Sign the packages
-dpkg-sig --sign builder ./output/*.deb
+# send debs to server
+rsync -azP --include './' --include '*.deb' --exclude '*' ./output/ ferreo@direct.pika-os.com:/srv/www/incoming/
 
-# Pull down existing ppa repo db files etc
-rsync -azqP --exclude '*.deb' ferreo@direct.pika-os.com:/srv/www/pikappa/ ./output/repo
+# add debs to repo
+ssh ferreo@direct.pika-os.com 'aptly repo add -force-replace -remove-files pika-main /srv/www/incoming/'
 
-wget -nv https://launchpad.net/ubuntu/+archive/primary/+files/reprepro_5.3.0-1.4_amd64.deb
-apt install -y ./reprepro_5.3.0-1.4_amd64.deb
-
-# Add the new package to the repo
-reprepro -V -C main --basedir ./output/repo/ includedeb lunar ./output/*.deb
-
-# Push the updated ppa repo to the server
-rsync -azqP ./output/repo/ ferreo@direct.pika-os.com:/srv/www/pikappa/
+# publish the repo
+ssh ferreo@direct.pika-os.com 'aptly publish update -batch -skip-contents -force-overwrite lunar filesystem:pikarepo:'
