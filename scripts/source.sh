2fd49cc4758d3ce835e4e4f574113d9b7958bc48
ferrreo <harderthanfire@gmail.com>
2024-03-16 10:46:26 +0000

    6.8.1 stable

--- scripts/source.sh
+++ scripts/source.sh
@@ -2,10 +2,7 @@
 
 echo "Pika Kernel - Getting source"
 
-#wget -nv https://cdn.kernel.org/pub/linux/kernel/v"$(echo $(cat ./VERSION) | cut -f1 -d".")".x/linux-"$(cat ./VERSION)".tar.gz
-#tar -xf ./linux-"$(cat ./VERSION)".tar.gz
-
-wget -nv https://git.kernel.org/torvalds/t/linux-6.8-rc6.tar.gz
+wget -nv https://cdn.kernel.org/pub/linux/kernel/v"$(echo $(cat ./VERSION) | cut -f1 -d".")".x/linux-"$(cat ./VERSION)".tar.gz
 tar -xf ./linux-"$(cat ./VERSION)".tar.gz
 
 cd linux-"$(cat ./VERSION)"
