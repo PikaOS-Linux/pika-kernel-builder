7377130b6743fe83bd302c9dfd6d154226890d40
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2024-07-21 18:55:14 +0300

    fix for PikaOS 4

--- release.sh
+++ release.sh
@@ -1,2 +1,2 @@
 # send debs to server
-rsync -azP --include './' --include '*.deb' --exclude '*' ./output/ ferreo@direct.pika-os.com:/srv/www/nest-incoming/
+rsync -azP --include './' --include '*.deb' --exclude '*' ./output/ ferreo@direct.pika-os.com:/srv/www/cockatiel-incoming/
